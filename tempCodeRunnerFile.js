b009dad5cf5c892be6bdd598563a2841fbdf76ca
jagritjain18-sudo <jagritjain18@gmail.in>
2026-07-31 15:05:01 +0530

    Removed unnecessary files

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-console.log(Object.keys(a));
-// console.log(Object.values(a));
-// console.log(Object.entries(a));
-// Object.keys(a).forEach((item,index)=>console.log(a[item],index));
-// Object.keys(a).forEach((_,index)=>console.log(index));//only index
-// const {name,city}=a;//destructuring of object
-// console.log(name);
